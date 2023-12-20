dbd5ead9fdde269b811ca8106a8f38de7644e3b1
Ken Vickie <153906165+vickieken@users.noreply.github.com>
2023-12-19 16:09:57 -0800

    age calculator  web app

---
+++ README.md
@@ -0,0 +1,89 @@
+# Frontend Mentor - Age calculator app solution
+
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View an age in years, months, and days after submitting a valid date through the form
+- Receive validation errors if:
+  - Any field is empty when the form is submitted
+  - The day number is not between 1-31
+  - The month number is not between 1-12
+  - The year is in the future
+  - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- **Bonus**: See the age numbers animate to their final number when the form is submitted
+
+### Screenshot
+
+![](./Screenshot%202023-12-20%20at%201.00.13%20AM.png)
+![](./Screenshot%202023-12-20%20at%2012.58.47%20AM.png)
+![](./Screenshot%202023-12-20%20at%2012.59.56%20AM.png)
+![](Screenshot%202023-12-20%20at%2012.59.56%20AM.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+-javaScript
+
+### What I learned
+
+In the course of this project I learnt how to make choose either of different numbers when using if statements, I also learnt how to get a positive number after calculation even when the given answer is a negative answer...
+
+Below are some of my code snippets:
+
+```html
+<h1>Some HTML code I'm proud of</h1>
+```
+```css
+.proud-of-this-css {
+  color: papayawhip;
+}
+```
+```js
+let value = 3;
+if ([1,2,3].includes(value)) {
+  return "value is either, 1, 2, 3";
+}
+
+let num = -5;
+let positiveNum = Math.abs(num);
+```
+
+### Continued development
+
+In my future projects I'd love to learn how to make my javScript code lesser and more beautiful
+
+### Useful resources
+
+## Author
+
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/@kenvickie)
---
+++ index.js
@@ -0,0 +1,178 @@
+const formItem = document.querySelectorAll('.form_list ');
+
+const labellingEl = document.querySelectorAll(".labelling")
+console.log(labellingEl);
+
+const wrapperEl = document.querySelector(".wrapper")
+console.log(wrapperEl);
+
+const btnEl = document.querySelector('.btn');
+
+const inputs = document.querySelectorAll('#input');
+
+const errorElements = document.querySelectorAll('.error_message');
+
+const years = document.querySelector('#years-ans');
+
+const months = document.querySelector('#months-ans');
+
+const days = document.querySelector('#days-ans');
+
+const currrentDate = new Date().getDate();
+
+const currentYear = new Date().getFullYear();
+
+const currentMonth = new Date().getMonth() + 1;
+
+const formListEL = [firstFrom, secondForm, ThirdForm] = formItem;
+
+const errorMessageEL = [dayError, monthError, yearError] = errorElements;
+
+const inputsEl = [dayInput, monthInput, YearInput] = inputs;
+
+const labellingEls = [labelDay, labelMonth, labelYear] = labellingEl;
+
+let hasError = false;
+
+let empty = false;
+
+btnEl.addEventListener("click", (e) => {
+    e.preventDefault();
+    // error for days
+    if(inputsEl[0].value == '') {
+        errorMessageEL[0].style.visibility = "visible";
+        errorMessageEL[0].style.opacity = 1;
+        errorMessageEL[0].innerHTML = "This field is required";
+        inputsEl[0].style.border = "1px solid red";
+        inputsEl[0].style.backgroundColor = "#ff53531c";
+        labellingEls[0].style.color = "red";
+        empty = true;
+        hasError = false;
+    }else if(inputsEl[0].value <= 0 || inputsEl[0].value > 31) {
+        errorMessageEL[0].style.visibility = "visible";
+        errorMessageEL[0].style.opacity = 1;
+        inputsEl[0].style.border = "1px solid red";
+        inputsEl[0].style.backgroundColor = "#ff53531c";
+        labellingEls[0].style.color = "red";
+        days.innerHTML = "- -";
+        hasError = true;
+        empty = false;
+        errorMessageEL[0].innerHTML = 'Must be a valid day'} else {
+        errorMessageEL[0].style.visibility = "hidden";
+        errorMessageEL[0].style.opacity = 0;
+        inputsEl[0].style.border = "1px solid #bcbcbc";
+        inputsEl[0].style.backgroundColor = "transparent";
+        empty = false;
+        hasError = true;
+        days.innerHTML = Math.abs(currrentDate - inputsEl[0].value);
+        days.style.opacity = 1.5;
+    }
+
+    // error for months
+    if(inputsEl[1].value == '') {
+        errorMessageEL[1].style.visibility = "visible";
+        errorMessageEL[1].style.opacity = 1;
+        errorMessageEL[1].innerHTML = "This field is required";
+        inputsEl[1].style.border = "1px solid red";
+        inputsEl[1].style.backgroundColor = "#ff53531c";
+        labellingEls[1].style.color = "red";
+        empty = true;
+        hasError = false;
+    }else if(inputsEl[1].value <= 0 || inputsEl[1].value > 12) {
+        errorMessageEL[1].style.visibility = "visible";
+        errorMessageEL[1].style.opacity = 1;
+        inputsEl[1].style.border = "1px solid red";
+        inputsEl[1].style.backgroundColor = "#ff53531c";
+        labellingEls[1].style.color = "red";
+        months.innerHTML = "- -";
+        hasError = true;
+        empty = false;
+        errorMessageEL[1].innerHTML = 'Must be a valid month'} else {
+        errorMessageEL[1].style.visibility = "hidden";
+        errorMessageEL[1].style.opacity = 0;
+        inputsEl[1].style.border = "1px solid #bcbcbc";
+        inputsEl[1].style.backgroundColor = "transparent";
+        empty = false;
+        hasError = true;
+        months.innerHTML = currentMonth - inputsEl[1].value;
+        months.style.opacity = 1;
+    }
+
+    // error for years
+    if(inputsEl[2].value == '') {
+        errorMessageEL[2].style.visibility = "visible";
+        errorMessageEL[2].style.opacity = 1;
+        errorMessageEL[2].innerHTML = "This field is required";
+        inputsEl[2].style.border = "1px solid red";
+        inputsEl[2].style.backgroundColor = "#ff53531c";
+        labellingEls[2].style.color = "red";
+        empty = true;
+        hasError = false;
+    }else if(inputsEl[2].value <= 0 || inputsEl[2].value > currentYear) {
+        errorMessageEL[2].style.visibility = "visible";
+        errorMessageEL[2].style.opacity = 1;
+        inputsEl[2].style.border = "1px solid red";
+        inputsEl[2].style.backgroundColor = "#ff53531c";
+        labellingEls[2].style.color = "red";
+        hasError = true;
+        empty = false;
+        errorMessageEL[2].innerHTML = 'must be in the past'} else {
+        errorMessageEL[2].style.visibility = "hidden";
+        errorMessageEL[2].style.opacity = 0;
+        inputsEl[2].style.border = "1px solid #bcbcbc";
+        inputsEl[2].style.backgroundColor = "transparent";
+        empty = false;
+        hasError = true;
+        years.innerHTML = currentYear - inputsEl[2].value;
+        years.style.opacity = 1;
+    }
+
+    // error februrary
+
+    if(inputsEl[1].value == 2 && inputsEl[0].value > 29) {
+        errorMessageEL[1].style.visibility = "visible";
+        errorMessageEL[1].style.opacity = 1;
+        errorMessageEL[0].style.visibility = "visible";
+        errorMessageEL[0].style.opacity = 1;
+        errorMessageEL[1].innerHTML = "Must be a valid month";
+        errorMessageEL[0].innerHTML = "Must be a valid day";
+        inputsEl[1].style.border = "1px solid red";
+        inputsEl[1].style.backgroundColor = "#ff53531c";
+        inputsEl[0].style.border = "1px solid red";
+        labellingEls[0].style.color = "red";
+        labellingEls[1].style.color = "red";
+        inputsEl[0].style.backgroundColor = "#ff53531c";
+        months.innerHTML = "- -";
+        months.style.opacity = .5;
+        days.innerHTML = "- -";
+        days.style.opacity = .5;
+    }
+
+    // error sept, april, june, nov
+
+    let monthsWith30Days = ["4", "6", "9", "11"];
+
+    if(monthsWith30Days.includes(inputsEl[1].value)  && inputsEl[0].value > 30) {
+        errorMessageEL[1].style.visibility = "visible";
+        errorMessageEL[1].style.opacity = 1;
+        errorMessageEL[0].style.visibility = "visible";
+        errorMessageEL[0].style.opacity = 1;
+        errorMessageEL[1].innerHTML = "Must be a valid month";
+        errorMessageEL[0].innerHTML = "Must be a valid day";
+        inputsEl[1].style.border = "1px solid red";
+        inputsEl[1].style.backgroundColor = "#ff53531c";
+        inputsEl[0].style.border = "1px solid red";
+        inputsEl[0].style.backgroundColor = "#ff53531c";
+        labellingEls[0].style.color = "red";
+        labellingEls[1].style.color = "red";
+        months.innerHTML = "- -";
+        months.style.opacity = .5;
+        days.innerHTML = "- -";
+        days.style.opacity = .5;
+    }
+})
+
+console.log(currrentDate);
+
+
+
